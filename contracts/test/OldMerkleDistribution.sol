9bb25877577e4fb45003f5e40c80b82c1ce9e7e7
Manuel Montenegro <manuel@nucypher.com>
2024-08-29 12:54:42 +0200

    Rename Old Merkle contract

--- contracts/test/OldMerkleDistribution.sol
+++ contracts/test/OldMerkleDistribution.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract OldMerkleDistributor is Ownable {
+contract OldMerkleDistribution is Ownable {
     using SafeERC20 for IERC20;
     using MerkleProof for bytes32[];
 
