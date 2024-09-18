3adeec5b555ca81b6fd9245748b3162b4a072a8a
Manuel Montenegro <manuel@nucypher.com>
2024-09-18 13:57:54 +0200

    Add assertion to claimMerkle function

--- test/ForkDeployment.test.js
+++ test/ForkDeployment.test.js
@@ -18,11 +18,11 @@ describe("Deployment of RewardsAggregator using mainnet network fork", function 
 
   it("should be deployed RewardsAggregator", async function () {
     const {
-      tokenContract,
-      tacoApp,
-      oldCumulativeMerkleDrop,
-      merkleRewardsHolder,
-      owner,
+      tokenContract: tokenAddr,
+      tacoApp: tacoAppAddr,
+      oldCumulativeMerkleDrop: oldCumulativeMerkleDropAddr,
+      merkleRewardsHolder: merkleRewardsHolderAddr,
+      owner: ownerAddr,
     } = await getNamedAccounts()
 
     // This staking prov is going to be used for tests. It has merkle-claimed
@@ -30,46 +30,82 @@ describe("Deployment of RewardsAggregator using mainnet network fork", function 
     const stakingProv = "0x0154C52ec5b6a3010758dDe78079589E67526767"
     const claim = getLastMerkleClaim(stakingProv)
 
+    const tokenContract = await ethers.getContractAt("TokenMock", tokenAddr)
+
     const RewardsAggregator = await ethers.getContractFactory(
       "RewardsAggregator"
     )
-
+    // Step1: deployment of the contract
     const rewardsAggregator = await RewardsAggregator.deploy(
-      tokenContract,
-      tacoApp,
-      oldCumulativeMerkleDrop,
-      merkleRewardsHolder,
-      owner
+      tokenAddr,
+      tacoAppAddr,
+      oldCumulativeMerkleDropAddr,
+      merkleRewardsHolderAddr,
+      ownerAddr
     )
 
-    const ownerSigner = await ethers.getSigner(owner)
+    const ownerSigner = await ethers.getSigner(ownerAddr)
+    const merkleRewardsHolderSigner = await ethers.getSigner(
+      merkleRewardsHolderAddr
+    )
 
-    await helpers.setBalance(owner, 10n ** 18n)
+    await helpers.setBalance(ownerAddr, 10n ** 18n)
+    await helpers.setBalance(merkleRewardsHolderAddr, 10n ** 18n)
+    // Step2: set the Merkle root
     await rewardsAggregator.connect(ownerSigner).setMerkleRoot(claim.merkleRoot)
 
+    // Step3: set ClaimableRewards contract allowance so RewardsAggregator can
+    // pull tokens and to push them to the stake beneficiary. The old Merkle
+    // contract allowance must be set to 0 and the same amount must be set in
+    // the new RewardsAggregator contract.
+    // await tokenContract.connect(merkleRewardsHolder)
+    await tokenContract
+      .connect(merkleRewardsHolderSigner)
+      .approve(rewardsAggregator.address, 1000000n * 10n ** 18n)
+
     // Check if the contract was successfully deployed
-    expect(await rewardsAggregator.owner()).to.equal(owner)
-    expect(await rewardsAggregator.token()).to.equal(tokenContract)
-    expect(await rewardsAggregator.application()).to.equal(tacoApp)
+    expect(await rewardsAggregator.owner()).to.equal(ownerAddr)
+    expect(await rewardsAggregator.token()).to.equal(tokenAddr)
+    expect(await rewardsAggregator.application()).to.equal(tacoAppAddr)
     expect(await rewardsAggregator.merkleRoot()).to.equal(claim.merkleRoot)
     expect(await rewardsAggregator.merkleRewardsHolder()).to.equal(
-      merkleRewardsHolder
+      merkleRewardsHolderAddr
     )
     expect(await rewardsAggregator.oldCumulativeMerkleDrop()).to.equal(
-      oldCumulativeMerkleDrop
+      oldCumulativeMerkleDropAddr
     )
 
     const claimed = await rewardsAggregator.cumulativeMerkleClaimed(stakingProv)
     expect(claimed).to.be.greaterThan(ethers.BigNumber.from(0))
 
     // Check if it is possible to claim Merkle rewards
-    // const prevBalance = await
+    const prevBalance = await tokenContract.balanceOf(claim.beneficiary)
     const toBeClaimed = ethers.BigNumber.from(claim.amount).sub(claimed)
+    const expectedBalance = prevBalance.add(toBeClaimed)
 
-    await rewardsAggregator.claimMerkle(stakingProv, claim.beneficiary, claim.amount, claim.merkleRoot, claim.proof)
+    await rewardsAggregator.claimMerkle(
+      stakingProv,
+      claim.beneficiary,
+      claim.amount,
+      claim.merkleRoot,
+      claim.proof
+    )
 
-    console.log(toBeClaimed)
-    // TODO: can I make a new claim?
+    expect(expectedBalance).to.equal(
+      await tokenContract.balanceOf(claim.beneficiary)
+    )
+
+    // should not be possible to get the same Merkle claim twice
+    await expect(
+      rewardsAggregator.claimMerkle(
+        stakingProv,
+        claim.beneficiary,
+        claim.amount,
+        claim.merkleRoot,
+        claim.proof
+      )
+    ).to.be.revertedWith("Nothing to claim")
+
     // TODO: It is needed to allow RewardsAggregator to pull tokens from ClaimableRewards
 
     // TODO: test a TACoApp claim
